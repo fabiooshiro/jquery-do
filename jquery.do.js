b5aeef56e75b6b907a8716fc9c0adbe278522a54
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2011-08-15 17:27:45 -0300

    Quando selecionava um item com o teclado, executava o primeiro item também

--- jquery.do.js
+++ jquery.do.js
@@ -1,29 +1,30 @@
 $(function(){
-            	
-	$("body").append(
+                
+    $("body").append(
         '<div id="jqueryDo" title="jQuery Do">' +
-    		'<div>jQuery-do: <input id="jqDoIn" type="text" /></div>' +
+            '<div>jQuery-do: <input id="jqDoIn" type="text" /></div>' +
         '</div>'
     );
-        	
-	$( "#jqueryDo" ).dialog({
-    	autoOpen:false,
-		height: 200,
-		width: 350
-	});
+
+    $( "#jqueryDo" ).dialog({
+        autoOpen:false,
+        height: 200,
+        width: 350
+    });
+    var selectedOne = false;
     var loaded = false;
     function doAction(el){
         if(el.nodeName=='A' || el.nodeName=='a'){ 
-		    var r = true;
-		    try{
-		   	    var r = $(el).triggerHandler('click');
-		       	if(r == undefined){
-		           	r = true;
-		        }
-		    }catch(e){}
-		    if(r !== false){// se o click nao retornou false, acessamos o href
-			    document.location.href = el.href;
-			}
+            var r = true;
+            try{
+                var r = $(el).triggerHandler('click');
+                if(r == undefined){
+                    r = true;
+                }
+            }catch(e){}
+            if(r !== false){// se o click nao retornou false, acessamos o href
+                document.location.href = el.href;
+            }
         }else if(el.nodeName == 'LABEL'){
             console.log('label for='+$(el).attr('for'));
             $('#' + $(el).attr('for')).focus();
@@ -33,22 +34,22 @@ $(function(){
         }
     }
     function ctrlSpaceHandler(){
-    	// start jQuery do inspirado no gnome-do
-    	if(loaded==false){
-        	loaded = true;
-        	var ls = $.cookie('jqDoClickedLs');
-	        if(ls == null){
-		        ls = new Array();
-		    }else{
-			    ls = ls.split(',');
-			}
-		    console.log(ls);
-        	var txt = new Array();
-        	$("a").each(function(){
-	        	var lbl = $.trim($(this).text());
-	        	var item = {label: lbl, value: this};
-        		txt.push(item);
-	        });
+        // start jQuery do inspirado no gnome-do
+        if(loaded==false){
+            loaded = true;
+            var ls = $.cookie('jqDoClickedLs');
+            if(ls == null){
+                ls = new Array();
+            }else{
+                ls = ls.split(',');
+            }
+            console.log(ls);
+            var txt = new Array();
+            $("a").each(function(){
+                var lbl = $.trim($(this).text());
+                var item = {label: lbl, value: this};
+                txt.push(item);
+            });
             $(":button").each(function(){
                 var lbl = $.trim($(this).val());
                 var item = {label: lbl, value: this};
@@ -70,7 +71,7 @@ $(function(){
                 return pb - pa;
             });
             $("#jqDoIn").keyup(function(event){
-                if(event.keyCode=='13'){
+                if(event.keyCode=='13' && selectedOne==false){
                     var t = $('.ui-autocomplete > li:first').text();
                     for(var i=0;i < txt.length;i++){
                         if(txt[i].label==t){
@@ -79,36 +80,38 @@ $(function(){
                     }
                 }
             });
-        	$("#jqDoIn").autocomplete({
-		        source: txt,
-		        focus: function( event, ui ) {
-					return false; // nao troca o texto do input
-		        },
-		        select: function(event, ui) {
+            $("#jqDoIn").autocomplete({
+                source: txt,
+                focus: function( event, ui ) {
+                    return false; // nao troca o texto do input
+                },
+                select: function(event, ui) {
+                    selectedOne = true;
                     var tmp = new Array();
-			        var lbl = ui.item.label;
+                    var lbl = ui.item.label;
                     for(var i = 0; i<ls.length; i++){
                         if(ls[i]!=lbl){
                             tmp.push(ls[i]);
                         }
                     }
                     ls = tmp;
-			        ls.push(lbl);
-			        $.cookie('jqDoClickedLs', ls, {expires: 15});
-			        var el = ui.item.value;
-			        doAction(el);
+                    ls.push(lbl);
+                    $.cookie('jqDoClickedLs', ls, {expires: 15});
+                    var el = ui.item.value;
+                    doAction(el);
                     return false;
-			    }
-        	})
-        	.data( "autocomplete" )._renderItem = function( ul, item ) {
-				return $( "<li></li>" )
-					.data( "item.autocomplete", item )
-					.append( "<a>" + item.label + "</a>" )
-					.appendTo( ul );
-			};
+                }
+            })
+            .data( "autocomplete" )._renderItem = function( ul, item ) {
+                return $( "<li></li>" )
+                    .data( "item.autocomplete", item )
+                    .append( "<a>" + item.label + "</a>" )
+                    .appendTo( ul );
+            };
         }
-		$("#jqueryDo").dialog('open');
+        $("#jqueryDo").dialog('open');
+        selectedOne = false;
     }
-	$(window).bind('keydown', 'ctrl+space', ctrlSpaceHandler);
-	$("input[type=text]").bind('keydown', 'ctrl+space', ctrlSpaceHandler);
+    $(window).bind('keydown', 'ctrl+space', ctrlSpaceHandler);
+    $("input[type=text]").bind('keydown', 'ctrl+space', ctrlSpaceHandler);
 });
